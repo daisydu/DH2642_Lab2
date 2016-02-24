123b1d4fcb4b9e02a9e4948e5dccad20fb956dfd
daisydu <857635228@qq.com>
2016-02-24 16:46:55 +0100

    controller

---
+++ js/view/ViewController.js
@@ -0,0 +1,34 @@
+//ExampleViewController Object constructor
+var GuestNumberViewController = function(view, model) {
+
+ view.plusButton.click(function(){
+    model.setNumberOfGuests(model.getNumberOfGuests() + 1);
+ });
+ 
+ view.minusButton.click(function(){
+    model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+ });
+ 
+}
+
+var ListDishesViewController = function(view, model){
+  view.selectType.change(function(){
+  	//console.log(view.selectType.val());
+  	var dishType = view.selectType.val();
+  	model.setSelectedDish(dishType);
+  });
+   
+  view.search.click(function(){
+  	var filter = view.keyWords.val();
+  	model.setFilter(filter);
+  });
+
+  view.selectDish.click(function(){
+    
+  });
+}
+
+this.selectDish = function(){
+    console.log("I am click");
+  }
+
